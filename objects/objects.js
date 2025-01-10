9a285c83d9e934c01cbb3736c760f4fac8f06d17
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-01-10 16:03:26 -0300

    learning objects in JS

---
+++ objects/objects.js
@@ -0,0 +1,18 @@
+const name = "Madelaine";
+const lastName = undefined;
+const age = 30;
+const ocupation = "actor";
+const country = "USA";
+
+const pessoa = {
+    name: "Madelaine",
+    lastName: "Petsch",
+    age: 30,
+    ocupation: "actor",
+    country: "USA"
+};
+
+console.log(pessoa)
+
+//Caso eu queria colocar mais pessoas no meu objeto, eu posso usar um array. 
+//Ou seja, fazer uma lista com as informações sobre as pessoas.
