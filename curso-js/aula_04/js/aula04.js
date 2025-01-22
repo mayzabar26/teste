6d18c4945433d1eac97973504a27bdc3c9546143
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-01-22 16:54:04 -0300

    Como trabalhar com JS

---
+++ curso-js/aula_04/js/aula04.js
@@ -0,0 +1,12 @@
+window.alert("Minha primeira messagem!") // mensagem de alerta na tela
+
+window.confirm("Está gostando de JS?") // mensagem de confirmação
+
+window.prompt("Qual o seu nome?") //interação com o user
+
+
+/*
+window.alert("Minha primeira messagem!")
+window.confirm("Está gostando de JS?")
+window.prompt("Qual o seu nome?")
+*/
---
+++ curso-js/aula_05/js/aula05.js
@@ -0,0 +1,6 @@
+//Variáveis
+/*
+-- São espaços que armazenam dados.
+-- Esses dados podem ser números, strings, objetos, valores boolean (true, false)... 
+-- Os nomes das variáveis são chamadas de identificadores
+*/
