791c125484e89d64b2b10c2d937775ad826f1abf
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-01-22 18:29:53 -0300

    Variáveis no js

--- curso-js/aula_05/js/aula05.js
+++ curso-js/aula_05/js/aula05.js
@@ -4,3 +4,27 @@
 -- Esses dados podem ser números, strings, objetos, valores boolean (true, false)... 
 -- Os nomes das variáveis são chamadas de identificadores
 */
+
+//exemplos de variáveis
+
+let name = "Madelaine";
+let age = 30;
+let occupation = "actress";
+
+//Para descobrir quais tipo de de dados eu tenho na minha variável, eu posso usar o comando typeof (tipo de)
+
+typeof name; 
+typeof age;
+typeof occupation; 
+
+typeof "Madelaine"; 
+typeof 30;
+typeof "actress"; 
+
+typeof []; //Retorna object 
+typeof {}; //Retorna object 
+typeof function(){}; //Retorna function
+typeof NaN; //Retorna number
+typeof Infinity; //Retorna number
+typeof null; //Retorna object
+typeof undefined; //Retorna undefined
