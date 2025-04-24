7db740c986639f9ae8bc83f33d8b5557244d620f
Mayza Costa Barbosa <mayzabar26@gmail.com>
2025-04-24 17:44:05 -0300

    organizando a classe Animal

---
+++ C#/desafio_zoologico/Animal.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+
+public abstract class Animal 
+{
+    //Set variables
+    private string _name;
+    private int _age;
+
+
+    //Set constructors
+    public Animal(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+
+    //Getters and Setters
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public int GetAge()
+    {
+        return _age;
+    }
+
+
+    //Set methods
+    public abstract string GetSound();
+
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name {_name} | Age: {_age} years old.");
+    }
+}
